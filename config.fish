5c00c71092b0646e249078f2f1589e5de0aef69a
ATAv035pT Cloud User <av035p@wfe-eastus2-mgmt-jump-vm.wam1ncbf3ujudb0bbnygsx42yg.cx.internal.cloudapp.net>
2021-10-03 03:11:45 +0000

    Initial Commit

---
+++ config.fish
@@ -0,0 +1,17 @@
+
+# Automatically Starts ssh agent on Fish.
+# https://gitlab.com/kyb/fish_ssh_agent
+
+# Aliases. They make life easier.
+alias ls="ls -lrth --color=auto"
+alias k="kubectl"
+alias kube="kubectl"
+alias tf="terraform"
+alias contexts="k config get-contexts"
+alias use="k config use-context"
+
+export LS_COLORS="$LS_COLORS:ow=1;34:tw=1;34:"
+
+#export PATH=$PATH:$HOME/.linkerd2/bin
+#
+cd $HOME
