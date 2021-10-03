0439c63a0b7b8d5e00aa65d23f0c905bc73fe280
Ashish Verma <av035p@wfe-eastus2-mgmt-jump-vmss000004.wam1ncbf3ujudb0bbnygsx42yg.cx.internal.cloudapp.net>
2021-10-03 03:41:48 +0000

    Changes for Ubuntu 18.04

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -22,31 +22,16 @@ function fish_prompt
             git rev-parse --git-dir >/dev/null 2>&1
         end
 
-        function _hg_branch_name
-            echo (hg branch 2>/dev/null)
-        end
-
-        function _is_hg_dirty
-            echo (hg status -mard 2>/dev/null)
-        end
-
-        function _is_hg_repo
-            fish_print_hg_root >/dev/null
-        end
-
         function _repo_branch_name
-            _$argv[1]_branch_name
+            eval _$argv[1]_branch_name
         end
 
         function _is_repo_dirty
-            _is_$argv[1]_dirty
+            eval _is_$argv[1]_dirty
         end
 
         function _repo_type
-            if _is_hg_repo
-                echo 'hg'
-                return 0
-            else if _is_git_repo
+            if _is_git_repo
                 echo 'git'
                 return 0
             end
