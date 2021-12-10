3fcb8a14621dcfef8d24fe5c1c21cbb4dece20d2
Ashish Verma <av035p@wfe-eastus2-nprd-mgmt-jump-vmss000010.xiuaurz4nuge1on4rpmrhbgyjc.cx.internal.cloudapp.net>
2021-12-10 03:51:52 +0000

    script based solution

---
+++ history.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -R /home/av035p/.local/share/fish/* /data/jumpusers/av035p/.local/share/fish/
---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Clone repository.
+#
+git clone https://github.com/vermacodes/ubuntu-fish-config.git
+
+if [ ! -d .config/fish ]; then
+  mkdir -p .config/fish
+fi
+
+cp -R ./ .config/fish/
+
+#
+# Copy stuff from backup.
+#
+cp -R /data/jumpusers/av035p/.local/share/fish/* /home/av035p/.local/share/fish/
+
+#
+# Setup cronjob if it doesnt exist already
+#
+chmod 755 /home/av035p/ubuntu-fish-config/history.sh
+crontab -l > /home/av035p/cron_bkp
+echo "0 * * * * /home/av035p/ubuntu-fish-config/history.sh > /dev/null 2>&1" >> /home/av035p/cron_bkp
+crontab cron_bkp
+rm cron_bkp
